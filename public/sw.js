89add0e73f5f8efa8517f4fc7edb09b5eb5b0a25
primsajun <primsajunp.cs22@stellamaryscoe.edu.in>
2026-08-03 15:48:07 +0530

    dappa

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+﻿// Basic Service Worker
+const CACHE_NAME = 'fcms-cache-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Pass through everything, this just satisfies the PWA install requirement
+  event.respondWith(fetch(event.request));
+});
